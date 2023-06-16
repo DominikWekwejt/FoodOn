18e057c16477d2fa0f1d2a8d0d153a2adf23f073
DominikWekwejt <nakae72@gmail.com>
2023-06-16 17:48:22 +0200

    Initial commit

---
+++ app/src/main/java/my/foodon/app/Chef.java
@@ -0,0 +1,13 @@
+package my.foodon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Chef extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
